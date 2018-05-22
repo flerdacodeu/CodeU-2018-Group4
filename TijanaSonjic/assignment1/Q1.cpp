6bdc6ae1a5c7627d8471948d9ed69c4faec98e75
Tijana Sonjic <38541577+TijanaSonjic@users.noreply.github.com>
2018-05-22 12:42:16 +0200

    First iteration of assigment1
    Correction of first assignment.

--- TijanaSonjic/assignment1/Q1.cpp
+++ TijanaSonjic/assignment1/Q1.cpp
@@ -8,10 +8,20 @@
 using namespace std;
 
 bool isWord(string word) {
-	for (int i = 0; i < word.length(); i++)
-		if (!((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z')))
+	for each (char letter in word){
+		if (!((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')))
 			return false;
+	}
+
 	return true;
+}
+
+void makeSmallLetter(char* letter) {
+	*letter = *letter + 'a' - 'A';
+}
+
+bool isUpperCase(char letter) {
+	return letter >= 'A' && letter <= 'Z';
 }
 
 // Function that checks whether two words are anagram of each other.
@@ -29,26 +39,21 @@ bool isAnagram(string word1, string word2, bool caseSensitive) {
 	for (int i = 0; i < word1.length(); i++) {
 		char letter = word1[i];
 
-		if (!caseSensitive && letter < 'a')
-			letter += 'a' - 'A';		
-			
-		auto it = lettersDifCount.find(letter);
-		if (it != lettersDifCount.end())
-			it->second++;
-		else
-			lettersDifCount.insert(lettersDifCount.end(), pair<char, int>(letter, 1));
+		if (!caseSensitive && isUpperCase(letter))
+			makeSmallLetter(&letter);
+
+		lettersDifCount[letter]++;
 	}
 
 	for (int i = 0; i < word2.length(); i++) {
 		char letter = word2[i];
 		
-		if (!caseSensitive && letter < 'a')
-			letter += 'a' - 'A';
+		if (!caseSensitive && isUpperCase(letter))
+			makeSmallLetter(&letter);
 
-		auto it = lettersDifCount.find(letter);
-		if (it != lettersDifCount.end() && it->second > 0)
-			it->second--;
-		else
+		lettersDifCount[letter]--;
+
+		if (lettersDifCount[letter] < 0)
 			return false;
 	}
 
@@ -58,7 +63,7 @@ bool isAnagram(string word1, string word2, bool caseSensitive) {
 
 vector<string> splitWordsBySpace(string sentence) {
 	istringstream iss(sentence);
-	vector<string> words{ istream_iterator<string>{iss}, istream_iterator<string>{}};
+	vector<string> words(istream_iterator<string>{iss}, istream_iterator<string>{});
 
 	return words;
 }
@@ -77,6 +82,14 @@ string generateKey(string word, bool caseSensitive) {
 	return word;
 }
 
+void updateCounters(unordered_map<string, int>* wordsDifCount, string word, bool caseSensitive, int inc, bool* continueAlgorithm) {
+	string key = generateKey(word, caseSensitive);
+	(*wordsDifCount)[key] += inc;
+
+	if ((*wordsDifCount)[key] < 0)
+		*continueAlgorithm = false;
+}
+
 // This function checks whether two sentences are anagram of each other.
 // With third argument you can choose if you want to handle case sensitive or case insensitive anagrams.
 //
@@ -87,29 +100,21 @@ bool areTwoSentencesAnagrams(string sentence1, string sentence2, bool caseSensit
 	vector<string> words1 = splitWordsBySpace(sentence1);
 	vector<string> words2 = splitWordsBySpace(sentence2);
 
+	bool continueAlgorithm = true;
+
 	if (words1.size() != words2.size())
 		return false;
 
 	unordered_map<string, int> wordsDifCount;
 
 	for (int i = 0; i < words1.size(); i++) {
-		string key = generateKey(words1[i], caseSensitive);
-
-		auto it = wordsDifCount.find(key);
-		if (it != wordsDifCount.end())
-			it->second++;
-		else
-			wordsDifCount.insert(wordsDifCount.end(), pair<string, int>(key, 1));
+		updateCounters(&wordsDifCount, words1[i], caseSensitive, 1, &continueAlgorithm);
 	}
 
 	for (int i = 0; i < words2.size(); i++) {
-		string key = generateKey(words2[i], caseSensitive);
+		updateCounters(&wordsDifCount, words2[i], caseSensitive, -1, &continueAlgorithm);
 
-		auto it = wordsDifCount.find(key);
-
-		if (it != wordsDifCount.end() && it->second>0)
-			it->second--;
-		else
+		if (!continueAlgorithm)
 			return false;
 	}
 
@@ -125,7 +130,7 @@ int main() {
 	string sentence1 = "Today is a great day";
 	string sentence2 = "Is a treag toayd ayd";
 
-	if (isAnagram(word1, word2, 0))
+	if (isAnagram(word1, word2, 1))
 		cout << "Words " << word1 << " and " << word2 << " are anagrams." << endl;
 	else
 		cout << "Words " << word1 << " and " << word2 << " are not anagrams." << endl;
