722a916fd7322d4b67b71afc0a0a77cd55ce5b30
Anna Kopeliovich <nufan@list.ru>
2018-05-27 13:09:38 +0300

    Add converting to unsigned char

--- annnufan/assignment1/solution_1.cpp
+++ annnufan/assignment1/solution_1.cpp
@@ -18,12 +18,12 @@ bool word_anagram(std::string word_1, std::string word_2, bool case_sens) {
 	std::vector<int> count(256, 0);
 	
 	//String used signed char. So, it needs to add 127 for each char for counting sort 
-	for (auto ch : word_1) {
-		count[ch + 127]++;
+	for (char ch : word_1) {
+		count[(unsigned char)ch]++;
 	}
-	for (auto ch : word_2) {
-		if (count[ch + 127] > 0) {
-			count[ch + 127]--;
+	for (char ch : word_2) {
+		if (count[(unsigned char)ch] > 0) {
+			count[(unsigned char)ch]--;
 		} else {
 			return false;
 		}
