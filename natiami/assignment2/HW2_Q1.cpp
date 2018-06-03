c5042ff560c36a51ec98e75a06c5f270a6944387
natiami <natia.doliashvili@ens.tsu.edu.ge>
2018-06-03 15:54:39 +0300

    solutions for Q1 and Q2

---
+++ natiami/assignment2/HW2_Q1.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+#include<algorithm>
+#include<array> 
+#include<queue>
+
+using namespace std;
+
+template<typename T>
+struct Node
+{
+	T key;
+	Node *left, *right, *parent;
+
+	Node() {
+		this->left = NULL;
+		this->right = NULL;
+		this->parent = NULL;
+	}
+
+	Node(T key, Node* parent) :Node() {
+		this->key = key;
+		this->parent = parent;
+	}
+	Node(T key, Node* left, Node* right, Node* parent) {
+		this->key = key;
+		this->left = left;
+		this->right = right;
+		this->parent = parent;
+	}
+};
+
+template<typename T>
+Node<T>* search(Node<T>* head, T key)
+{
+	if (head == NULL){
+		cout << "Tree is empty";
+		return NULL;
+	}
+
+	Node<T>* current_node = head;
+	queue<Node<T>* > unvisited;
+	unvisited.push(current_node);
+
+	while (unvisited.empty() != true)
+	{
+		current_node = unvisited.front();
+		unvisited.pop();
+		if (current_node->key == key) return current_node;
+		if (current_node->left != NULL) unvisited.push(current_node->left);
+		if (current_node->right != NULL) unvisited.push(current_node->right);
+	}
+	return NULL;
+}
+
+template<typename T>
+void print_ancestors(Node<T>* head, T key){
+	Node<int>* node = search(head, key);
+	if (node == head) cout << "This node is the root";
+	else if (node != NULL){
+		// Print the ancestors
+		while (node->parent != NULL){
+			cout << node->parent->key << " ";
+			node = node->parent;
+		}
+	}
+	else cout << "Could not find a node with this key " << key;
+	cout << endl;
+}
+
+int main(){
+
+	// Node itself is condisered to be its ancestor
+	Node<int>* tree = new Node<int>();
+	tree->key = 7;
+	tree->left = new Node<int>(3, tree);
+	tree->right = new Node<int>(4, tree);
+	tree->left->left = new Node<int>(2, tree->left);
+	tree->left->right = new Node<int>(5, tree->left);
+	tree->right->right = new Node<int>(8, tree->right);
+	tree->left->left->left = new Node<int>(1, tree->left->left);
+	tree->left->left->right = new Node<int>(6, tree->left->left);
+
+	int key;
+	cout << "Input the key to print its ancestors" << endl;
+	cin >> key;
+	print_ancestors(tree, key);
+
+	return 0;
+}
---
+++ natiami/assignment2/HW2_Q2.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+#include<algorithm>
+#include<array> 
+#include<set>
+#include<queue>
+
+using namespace std;
+
+template<typename T>
+struct Node
+{
+	T key;
+	Node *left, *right, *parent;
+
+	Node() {
+		this->left = NULL;
+		this->right = NULL;
+		this->parent = NULL;
+	}
+
+	Node(T key, Node* parent) :Node() {
+		this->key = key;
+		this->parent = parent;
+	}
+	Node(T key, Node* left, Node* right, Node* parent) {
+		this->key = key;
+		this->left = left;
+		this->right = right;
+		this->parent = parent;
+	}
+};
+
+template<typename T>
+Node<T>* search(Node<T>* head, T key)
+{
+	if (head == NULL){
+		cout << "Tree is empty";
+		return NULL;
+	}
+
+	Node<T>* current_node = head;
+	queue<Node<T>* > unvisited;
+	unvisited.push(current_node);
+
+	while (unvisited.empty() != true)
+	{
+		current_node = unvisited.front();
+		unvisited.pop();
+		if (current_node->key == key) return current_node;
+		if (current_node->left != NULL) unvisited.push(current_node->left);
+		if (current_node->right != NULL) unvisited.push(current_node->right);
+	}
+	return NULL;
+}
+
+template<typename T>
+set<Node<T>* > findPathToRoot(Node<T>* node){
+	set<Node<T>* > path_to_root;
+	Node<T>* current_node = node;
+	while (current_node != NULL){
+		path_to_root.insert(current_node);
+		current_node = current_node->parent;
+	}
+	return path_to_root;
+}
+
+template<typename T>
+Node<T>* findLowestCommonAncestor(Node<T>* node1, Node<T>* node2){
+	// Node itself is condisered to be its ancestor
+
+	set<Node<T>* > path_to_root = findPathToRoot(node1);
+	Node<T>* current_node = node2;
+	while (current_node != NULL){
+		if (path_to_root.find(current_node) != path_to_root.end())
+			return current_node;
+		current_node = current_node->parent;
+	}
+}
+
+int main(){
+
+	// Example tree from assignment
+	Node<int>* tree = new Node<int>();
+	tree->key = 7;
+	tree->left = new Node<int>(3, tree);
+	tree->right = new Node<int>(4, tree);
+	tree->left->left = new Node<int>(2, tree->left);
+	tree->left->right = new Node<int>(5, tree->left);
+	tree->right->right = new Node<int>(8, tree->right);
+	tree->left->left->left = new Node<int>(1, tree->left->left);
+	tree->left->left->right = new Node<int>(6, tree->left->left);
+
+	cout << "Input two nodes to find their lowest common ancestor" << endl;
+	int key1, key2;
+	cin >> key1 >> key2;
+
+	Node<int>* node1 = search(tree, key1);
+	Node<int>* node2 = search(tree, key2);
+
+	Node<int>* lowestCommonAncestor = findLowestCommonAncestor(node1, node2);
+	cout << "Lowest Common Ancestor is " << lowestCommonAncestor->key << endl;
+
+	return 0;
+}
