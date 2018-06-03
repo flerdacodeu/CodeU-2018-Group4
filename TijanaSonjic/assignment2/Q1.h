ff456859f816bbbf0d1640c83ae745b378697456
Tijana Sonjic <38541577+TijanaSonjic@users.noreply.github.com>
2018-06-03 13:00:37 +0200

    Solution for Q1 task of second assignment

---
+++ TijanaSonjic/assignment2/Q1.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Node{
+private:
+	Node* leftChild;
+	Node* rightChild;
+	int info;
+public:
+	Node(int info, Node* leftChild = nullptr, Node* rightChild = nullptr) {
+		this->info = info;
+		this->leftChild = leftChild;
+		this->rightChild = rightChild;
+	}
+	
+	~Node() {
+		// TODO
+	}
+	
+	Node* insertLeftChild(Node* node) {
+		this->leftChild = node;
+
+		return this;
+	}
+
+	Node* insertRightChild(Node* node) {
+		this->rightChild = node;
+
+		return this;
+	}
+
+	int getInfo() {
+		return info;
+	}
+
+	Node* getLeftChild() {
+		return leftChild;
+	}
+
+	Node* getRightChild() {
+		return rightChild;
+	}
+};
+
+// Function will return false if key is not in the tree.
+//
+bool printAncestors(Node* root, int key) {
+	if (!root)
+		return false;
+
+	if (root->getInfo() == key)
+		return true;
+
+	if (printAncestors(root->getLeftChild(), key) || printAncestors(root->getRightChild(), key)) {
+		cout << root->getInfo() << " ";
+		return true;
+	}
+
+	return false;
+}
+
+
