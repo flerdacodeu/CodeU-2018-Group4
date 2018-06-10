51fc6bab88679547fe54490fdc4020c2775addae
ayeletK <kravkrav@gmail.com>
2018-06-10 09:26:26 +0300

    Update
    Main changes:
    - remove redundant if case in method "get_node_from_path"
    - update logic in method "get_path_string" - remove parent_node parameter. Handling the path string extension after folding from the recursive call.
    - add method "get_nodes_paths"
    - add parameters check at method "get_lowest_common_ancestor"

--- ayeletK/assignment2/Q2_solution.cpp
+++ ayeletK/assignment2/Q2_solution.cpp
@@ -1,71 +1,71 @@
 #include "tree_node.cpp"
+#include "my_exceptions.cpp"
 #include <string>
 
 using namespace std;
 
-#define KEY_NOT_IN_PATH "-"
+#define PATH_EXISTS true
+#define PATH_DOESNT_EXISTS false
 #define EMPTY_PATH ""
 #define LEFT_SON '0'
 #define RIGHT_SON '1'
 
 template <typename Key>
-char get_addition_to_path(Node<Key>* son_node, Node<Key>* parent_node){
-  return parent_node->left == son_node ? LEFT_SON : RIGHT_SON;
-}
-
-template <typename Key>
-string get_path_string(Key key, Node<Key>* curr_node, Node<Key>* parent_node = NULL){
-  if (!curr_node) return KEY_NOT_IN_PATH;
+bool get_path_string(Key key, Node<Key>* curr_node, string* path){
+  if (!curr_node) return PATH_DOESNT_EXISTS;
   Key curr_key = curr_node->get_key();
-  // EMPTY_PATH represents the case the root node is the key
   if (curr_key == key){
-    if (!parent_node) return EMPTY_PATH;
-    else return string(1, get_addition_to_path(curr_node, parent_node));
+    *path = EMPTY_PATH; // EMPTY_PATH represents the case the root node is the key
+    return PATH_EXISTS;
   }
-  string res;
-  res = get_path_string(key, curr_node->left, curr_node);
-  if (res == KEY_NOT_IN_PATH){
-    res = get_path_string(key, curr_node->right, curr_node);
+  bool path_exists = get_path_string(key, curr_node->left, path);
+  if (path_exists){
+      path->insert(path->begin(), LEFT_SON);
+      return PATH_EXISTS;
   }
-  if (res != KEY_NOT_IN_PATH && parent_node){
-    char addition = get_addition_to_path(curr_node, parent_node);
-    res.insert(res.begin(), addition);
+  path_exists = get_path_string(key, curr_node->right, path);
+  if (path_exists){
+      path->insert(path->begin(), RIGHT_SON);
+      return PATH_EXISTS;
   }
-  return res;
+  return PATH_DOESNT_EXISTS;
 }
 
 template <typename Key>
 Node<Key>* get_node_from_path(Node<Key>* root, string path){
-  if (!root) throw "the given tree is empty.";
-  if (path == KEY_NOT_IN_PATH) return NULL;
-  if (path == EMPTY_PATH) return root;
+  if (!root) throw emptyTreeException();
   Node<Key>* curr_node = root;
   for (char c: path){
-    if (c == LEFT_SON) curr_node = curr_node->left;
-    else curr_node = curr_node->right;
-    if (!curr_node) throw "Illegal path";
+      if (c == LEFT_SON) curr_node = curr_node->left;
+      else if (c == RIGHT_SON) curr_node = curr_node->right;
+      else throw illegalPathException(); // case there is illegal char in path string
+      if (!curr_node) throw illegalPathException(); // case path leads to null node
   }
   return curr_node;
 }
 
 template <typename Key>
+void get_nodes_paths(Node<Key>* root, Node<Key>* node1, Node<Key>* node2, string* path1, string* path2){
+    bool node1_path_exists = get_path_string(node1->get_key(), root, path1);
+    if (!node1_path_exists) throw nodeNotExistsException("node1");
+    bool node2_path_exists = get_path_string(node2->get_key(), root, path2);
+    if (!node2_path_exists) throw nodeNotExistsException("node2");
+}
+
+template <typename Key>
 Node<Key>* get_lowest_common_ancestor(Node<Key>* root, Node<Key>* node1, Node<Key>* node2){
-  string node1_path = get_path_string(node1->get_key(), root);
-  if (node1_path == KEY_NOT_IN_PATH) throw "node1 does not exists in the given tree.";
-  string node2_path = get_path_string(node2->get_key(), root);
-  if (node2_path == KEY_NOT_IN_PATH) throw "node2 does not exists in the given tree.";
+  if (!root) throw emptyTreeException();
+  if (!node1 || !node2) throw nullNodeException();
+  string node1_path, node2_path;
+  get_nodes_paths(root, node1, node2, &node1_path, &node2_path);
   int size1 = node1_path.size();
   int size2 = node2_path.size();
   int i;
   for (i = 0; i < size1 && i < size2; ++i){
-    char c1 = node1_path.at(i);
-    char c2 = node2_path.at(i);
-    if (c1 != c2){
-      return get_node_from_path(root, node1_path.substr(0, i));
-    }
+      char c1 = node1_path.at(i);
+      char c2 = node2_path.at(i);
+      if (c1 != c2) return get_node_from_path(root, node1_path.substr(0, i));
   }
-  if (i == size1){
-    return node1;
-  }
+  if (i == size1) return node1;
   return node2;
 }
