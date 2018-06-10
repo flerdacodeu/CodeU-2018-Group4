556dcfed924c70c0ea4fa8ca1fc4079687c82db5
Tijana Sonjic <38541577+TijanaSonjic@users.noreply.github.com>
2018-06-10 10:54:50 +0200

    Update Q2.h

--- TijanaSonjic/assignment2/Q2.h
+++ TijanaSonjic/assignment2/Q2.h
@@ -1,58 +1,6 @@
 #pragma once
-#include <iostream>
-using namespace std;
+#include "Node.h"
 
-class Node {
-private:
-	Node* leftChild;
-	Node* rightChild;
-	int info;
-public:
-	Node(int info, Node* leftChild = nullptr, Node* rightChild = nullptr) {
-		this->info = info;
-		this->leftChild = leftChild;
-		this->rightChild = rightChild;
-	}
-
-	void distroy() {
-		if (leftChild) {
-			leftChild->distroy();
-			leftChild = nullptr;
-		}
-		if (rightChild) {
-			rightChild->distroy();
-			rightChild = nullptr;
-		}
-	}
-
-	~Node() {
-		distroy();
-	}
-
-	Node* insertLeftChild(Node* node) {
-		this->leftChild = node;
-
-		return this;
-	}
-
-	Node* insertRightChild(Node* node) {
-		this->rightChild = node;
-
-		return this;
-	}
-
-	int getInfo() {
-		return info;
-	}
-
-	Node* getLeftChild() {
-		return leftChild;
-	}
-
-	Node* getRightChild() {
-		return rightChild;
-	}
-};
 
 bool isInTheTree(Node* root, Node* node) {
 	if (!root)
@@ -64,15 +12,18 @@ bool isInTheTree(Node* root, Node* node) {
 	return isInTheTree(root->getLeftChild(), node) || isInTheTree(root->getRightChild(), node);
 }
 
-Node* findFirstCommonAncestor(Node* root, Node* node1, Node* node2) {
+// This is a helper function, do not call directly. Call firstCommonAncestor instead.
+// Function assumes that both nodes are in the tree.
+//
+Node* findFirstCommonAncestorAux(Node* root, Node* node1, Node* node2) {
 	if (!root)
 		return nullptr;
 
 	if (isInTheTree(root->getLeftChild(), node1) && isInTheTree(root->getLeftChild(), node2))
-		return findFirstCommonAncestor(root->getLeftChild(), node1, node2);
+		return findFirstCommonAncestorAux(root->getLeftChild(), node1, node2);
 
 	if (isInTheTree(root->getRightChild(), node1) && isInTheTree(root->getRightChild(), node2))
-		return findFirstCommonAncestor(root->getRightChild(), node1, node2);
+		return findFirstCommonAncestorAux(root->getRightChild(), node1, node2);
 
 	return root;
 }
@@ -84,5 +35,5 @@ Node* firstCommonAncestor(Node* root, Node* node1, Node* node2) {
 	if (!isInTheTree(root, node1) || !isInTheTree(root, node2))
 		return nullptr;
 
-	return findFirstCommonAncestor(root, node1, node2);
+	return findFirstCommonAncestorAux(root, node1, node2);
 }
