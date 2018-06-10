db9f2668b91da6860e50f12d086d7974937f60c7
ayeletK <kravkrav@gmail.com>
2018-06-10 09:10:27 +0300

    Update
    change name "is_direct_ancestor" to "is_parent".
    add exceptions instead of throwing strings

--- ayeletK/assignment2/Q1_solution.cpp
+++ ayeletK/assignment2/Q1_solution.cpp
@@ -1,11 +1,12 @@
 #include "tree_node.cpp"
+#include "my_exceptions.cpp"
 #include <iostream>
 #include <list>
 
 using namespace std;
 
 template <typename Key>
-bool is_direct_ancestor(Key key, Node<Key>* curr_node){
+bool is_parent(Key key, Node<Key>* curr_node){
   bool left_son_has_key = 
     curr_node->has_left() && curr_node->left->get_key() == key;
   bool right_son_has_key = 
@@ -14,31 +15,37 @@ bool is_direct_ancestor(Key key, Node<Key>* curr_node){
 }
 
 template <typename Key>
-list<Key> get_all_ancestors(Key key, Node<Key>* curr_node){
-  list<Key> res;
-  // if the current node is null, leaf or has the given key, it's not ancestor
-  if (!curr_node || !curr_node->get_key() == key || curr_node->is_leaf()) return res;
-  if (is_direct_ancestor(key, curr_node)){
-    res.push_back(curr_node->get_key());
-    return res;
-  }
-  res = get_all_ancestors(key, curr_node->left);
-  if (res.empty()) res = get_all_ancestors(key, curr_node->right);
-  if (!res.empty()){
-    res.push_back(curr_node->get_key());
-    return res;
-  }
-  return res; // if reached here, key not found in the tree rooted with curr node.
+list<Key> get_path_to_root(Key key, Node<Key>* curr_node){
+    list<Key> res;
+    if (!curr_node) return res;
+    if (curr_node->get_key() == key){
+        res.push_back(curr_node->get_key());
+        return res;
+    }
+    res = get_path_to_root(key, curr_node->left);
+    if (res.empty()) res = get_all_ancestors(key, curr_node->right);
+    if (!res.empty()){
+        res.push_back(curr_node->get_key());
+        return res;
+    }
+    return res; // if reached here, key not found in the tree rooted with curr node.
+}
+
+template <typename Key>
+list<Key> get_all_ancestors(Key key, Node<Key>* root){
+    list<Key> path_res = get_path_to_root(key, root);
+    if (path_res.front() == key) path_res.pop_front();
+    return path_res; // now represents all ancestors list from bottom to root
 }
 
 template <typename Key>
 void print_all_ancestors(Key key, Node<Key>* root){
-  if (!root) throw "The given tree is empty.";
+  if (!root) throw emptyTreeException();
   list<Key> ancestors_keys = get_all_ancestors(key, root);
-  if (ancestors_keys.empty() && root->get_key() != key) throw "The given key does not exists in the given tree.";
+  if (ancestors_keys.empty() && root->get_key() != key) throw keyNotExistsException();
   typename list<Key>::iterator it;
   for (it = ancestors_keys.begin(); it != ancestors_keys.end(); ++it){
-    cout << *it << " ";
+      cout << *it << " ";
   }
   cout << endl;
 }
