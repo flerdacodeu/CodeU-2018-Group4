7ee71139cdf910dfcdbb995913f6daa82c5a29b5
ayeletK <kravkrav@gmail.com>
2018-06-10 09:00:33 +0300

    Update
    check explicitly for nullptr in methods "has_left" and "has_right"

--- ayeletK/assignment2/tree_node.cpp
+++ ayeletK/assignment2/tree_node.cpp
@@ -9,7 +9,7 @@ public:
   Node(Key key): Node(key, nullptr, nullptr) {}
   Node(Key key, Node* left, Node* right): key(key), left(left), right(right) {}
   Key get_key(){ return key; }
-  bool has_left(){ return left; }
-  bool has_right(){ return right; }
+  bool has_left(){ return left != nullptr; }
+  bool has_right(){ return right != nullptr; }
   bool is_leaf(){ return !has_left() && !has_right(); }
 };
