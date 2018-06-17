842ddade1ad2a61575b7adc9a4110a17b4fa66f5
Anna Kopeliovich <nufan@list.ru>
2018-06-17 18:00:07 +0300

    First iteration for 3 assignment

---
+++ annnufan/assignment3/dictionary.h
@@ -0,0 +1,35 @@
+#include <set>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+class dictionary{
+	std::set<std::string> words, prefixes;
+	
+	void find_all_prefixes() {
+		std::string prefix = "";
+		for (std::string word : words) {
+			prefix = "";
+			for (char c : word) {
+				prefix += c;
+				prefixes.insert(prefix);
+			}
+		}
+	}
+	
+public:
+	dictionary(std::set<std::string> w) : words(w) {
+		find_all_prefixes();
+	}
+
+	dictionary() {}
+
+	bool is_word(std::string word) {
+		return words.find(word) != words.end();
+	}
+	
+	bool is_prefix(std::string word) {
+		return prefixes.find(word) != prefixes.end();	
+	}
+	
+};
