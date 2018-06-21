7e22f00d28a9f97da1311049da33c5ffddc91f32
July-Morning <chistiakova.olga@gmail.com>
2018-06-21 14:49:21 +0300

    Update dictionary.hpp

--- July-Morning/assignment3/dictionary.hpp
+++ July-Morning/assignment3/dictionary.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #include <unordered_set>
 
 class Dictionary 
@@ -13,9 +14,12 @@ class Dictionary
         // Add word to dictionary
         void add_word (std::string w);
         
+        // Add vector of words to dictionary
+        void add_words (std::unordered_set<std::string> w_vec);
+        
         // Check if word is in dictionary
-        bool is_word (std::string w);
+        bool is_word (std::string w) const;
         
         // Check if given string is prefix of at least one word of the dictionary
-        bool is_prefix (std::string p);
+        bool is_prefix (std::string p) const;
 };
