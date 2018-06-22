e6f850b7e8cb1de775b314f0699e320e1fecd4a5
July-Morning <chistiakova.olga@gmail.com>
2018-06-22 15:22:52 +0300

    Iteration 2: update dictionary.cpp

--- July-Morning/assignment3/dictionary.cpp
+++ July-Morning/assignment3/dictionary.cpp
@@ -9,26 +9,25 @@ void Dictionary::add_word (std::string w)
     for (int i = 1; i <= w.size(); i++)
     {
         std::string p = w.substr(0, i); 
-        if (!is_prefix(p))
-            prefix_set.insert(p);
+        prefix_set.insert(p);
     }    
 }
 
 // Add vector of words to dictionary
-void Dictionary::add_words (std::unordered_set<std::string> w_vec)
+void Dictionary::add_words (const std::unordered_set<std::string>& w_vec)
 {
-    for (auto w : w_vec)
+    for (const auto& w : w_vec)
         add_word (w);
 }
 
 // Check if word is in dictionary
 bool Dictionary::is_word (std::string w) const
 {
-    return (word_set.find(w) != word_set.end());
+    return word_set.find(w) != word_set.end();
 }
         
 // Check if given string is prefix of at least one word of the dictionary
 bool Dictionary::is_prefix (std::string p) const
 {
-    return (prefix_set.find(p) != prefix_set.end());
+    return prefix_set.find(p) != prefix_set.end();
 }
