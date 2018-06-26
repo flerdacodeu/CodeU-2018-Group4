df0e55dfa461cb3d61668327e351e81bc93db36b
natiami <natia.doliashvili@ens.tsu.edu.ge>
2018-06-26 22:11:20 +0300

    inits changed

--- natiami/assignment3/FindWordsTest.cpp
+++ natiami/assignment3/FindWordsTest.cpp
@@ -6,46 +6,35 @@ void FindWordsTest::test(){
 	test_EmptyDictionary();
 }
 
-Grid* FindWordsTest::initGrid(){
-	string gridRow1 = "AAR";
-	string gridRow2 = "TCD";
-	Grid* grid = new Grid(3, 2);
-	for (size_t j = 0; j < grid->width; j++)
-	{
-		grid->cells[0][j] = gridRow1[j];
-		grid->cells[1][j] = gridRow2[j];
-	}
-	return grid;
+void FindWordsTest::initGrid(){
+	grid = new Grid(3, 2);
+	grid->cells = { { 'A', 'A', 'R' }, { 'T', 'C', 'D' } };
 }
 
-Dictionary* FindWordsTest::initDictionary(){
+void FindWordsTest::initDictionary(){
 	set<string> words = { "CAR", "CARD", "CART", "CAT" };
 	set<string> prefixes = { "C", "CA", "CAR", "CARD", "CART", "CAT" };
-	Dictionary* dictionary = new Dictionary(words, prefixes);
-	return dictionary;
+	dictionary = new Dictionary(words, prefixes);
 }
 
 void FindWordsTest::test_findAllWords(){
-	Dictionary* dictionary = initDictionary();
-	Grid* grid = initGrid();
-	set<string> found_words = grid->findAllWords(dictionary);
+	initDictionary();
+	initGrid();
 	set<string> expected_words = { "CAR", "CARD", "CAT" };
-	EXPECT_EQ(expected_words, found_words);
+	EXPECT_EQ(expected_words, grid->findAllWords(dictionary));
 }
 
 void FindWordsTest::test_EmptyGrid(){
-	Dictionary* dictionary = initDictionary();
-	Grid* grid = new Grid();
-	set<string> found_words = grid->findAllWords(dictionary);
-	set<string> expected_words = set<string>();
-	EXPECT_EQ(expected_words, found_words);
+	initDictionary();
+	Grid emptyGrid;
+	set<string> empty_expected_words;
+	EXPECT_EQ(empty_expected_words, emptyGrid.findAllWords(dictionary));
 }
 
 void FindWordsTest::test_EmptyDictionary(){
 	Dictionary* dictionary = new Dictionary();
-	Grid* grid = initGrid();
-	set<string> found_words = grid->findAllWords(dictionary);
+	initGrid();
 	set<string> expected_words = set<string>();
-	EXPECT_EQ(expected_words, found_words);
+	EXPECT_EQ(expected_words, grid->findAllWords(dictionary));
 }
 
