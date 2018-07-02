c55d3badc5d405da0af6ed82f0d535ccd1f704ad
natiami <natia.doliashvili@ens.tsu.edu.ge>
2018-07-03 00:34:30 +0400

    made to one-liners

--- natiami/assignment4/FindIslandsTest.cpp
+++ natiami/assignment4/FindIslandsTest.cpp
@@ -3,59 +3,48 @@
 void FindIslandsTest::test(){
 	test_findNumberOfIslands();
 	test_diagonalLands();
-	test_EmptyMapOfTiles();
+	test_EmptyisLandTile();
 	test_allWater();
 	test_allLand();
 }
 
 void FindIslandsTest::test_findNumberOfIslands(){
-	vector<vector<bool> > mapOfTiles{ { false, true, false, true },
+	vector<vector<bool> > isLandTile{ { false, true, false, true },
 								{ true, true, false, false },
 								{ false, false, true, false },
 								{ false, false, true, false } };
-	findIslands = FindIslands(4, 4, mapOfTiles);
 
-	int expected_nr_island = 3;
-	EXPECT_EQ(expected_nr_island, findIslands.findNumberOfIslands());
+	EXPECT_EQ(3, FindIslands(isLandTile).findNumberOfIslands());
 }
 
 void FindIslandsTest::test_diagonalLands(){
-	vector<vector<bool> > mapOfTiles{ { true, false, true, false },
+	vector<vector<bool> > isLandTile{ { true, false, true, false },
 										{ false, true, false, true },
 										{ true, false, true, false },
 										{ false, true, false, true } };
-	findIslands = FindIslands(4, 4, mapOfTiles);
 
-	int expected_nr_island = 8;
-	EXPECT_EQ(expected_nr_island, findIslands.findNumberOfIslands());
+	EXPECT_EQ(8, FindIslands(isLandTile).findNumberOfIslands());
 }
 
-void FindIslandsTest::test_EmptyMapOfTiles(){
-	vector<vector<bool> > mapOfTiles;
-	findIslands = FindIslands(0, 0, mapOfTiles);
-
-	int expected_nr_island = 0;
-	EXPECT_EQ(expected_nr_island, findIslands.findNumberOfIslands());
+void FindIslandsTest::test_EmptyisLandTile(){
+	vector<vector<bool> > isLandTile;
+	EXPECT_EQ(0, FindIslands(isLandTile).findNumberOfIslands());
 }
 
 void FindIslandsTest::test_allWater(){
-	vector<vector<bool> > mapOfTiles{ { false, false, false, false },
+	vector<vector<bool> > isLandTile{ { false, false, false, false },
 										{ false, false, false, false },
 										{ false, false, false, false },
 										{ false, false, false, false } };
-	findIslands = FindIslands(4, 4, mapOfTiles);
 
-	int expected_nr_island = 0;
-	EXPECT_EQ(expected_nr_island, findIslands.findNumberOfIslands());
+	EXPECT_EQ(0, FindIslands(isLandTile).findNumberOfIslands());
 }
 
 void FindIslandsTest::test_allLand(){
-	vector<vector<bool> > mapOfTiles{ { true, true, true, true },
+	vector<vector<bool> > isLandTile{ { true, true, true, true },
 										{ true, true, true, true },
 										{ true, true, true, true },
 										{ true, true, true, true } };
-	findIslands = FindIslands(4, 4, mapOfTiles);
 
-	int expected_nr_island = 1;
-	EXPECT_EQ(expected_nr_island, findIslands.findNumberOfIslands());
+	EXPECT_EQ(1, FindIslands(isLandTile).findNumberOfIslands());
 }
