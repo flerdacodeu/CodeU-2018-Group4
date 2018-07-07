80ec3115b23ddf8806c0238ac2aa3956b848a5a5
Tijana Sonjic <38541577+TijanaSonjic@users.noreply.github.com>
2018-07-07 20:52:31 +0200

    Update test.cpp

--- TijanaSonjic/assignment4/test.cpp
+++ TijanaSonjic/assignment4/test.cpp
@@ -4,9 +4,6 @@
 
 using namespace std;
 
-void testEqual(IslandCount* islandCount, int num) {
-	EXPECT_EQ(islandCount->countIslands(), num);
-}
 
 int main() {
 	bool** map = new bool*[4];
@@ -15,29 +12,20 @@ int main() {
 	map[2] = new bool[4]{ false, false, true, false };
 	map[3] = new bool[4]{ false, false, true, false };
 	
-	IslandCount* islandCount = new IslandCount(map, 4, 4);
-
-	testEqual(islandCount, 3);
-
-	
+	IslandCount islandCount(map, 4, 4);
+	EXPECT_EQ(islandCount.countIslands(), 3);
+		
 	// Test with null map
-	IslandCount* islandCountWithoutMap = new IslandCount(nullptr, 0, 0);
-
-	testEqual(islandCountWithoutMap, 0);
-
-
+	IslandCount islandCountWithoutMap(nullptr, 0, 0);
+	EXPECT_EQ(islandCountWithoutMap.countIslands(), 0);
+	
 	// Test with one island across th whole map
 	bool** mapWithOneIsland = new bool*[1];
 	mapWithOneIsland[0] = new bool[3]{ true, true, true };
+	IslandCount islandCountTest(mapWithOneIsland, 1, 3);
+	EXPECT_EQ(islandCountTest.countIslands(), 1);
 
-	IslandCount* islandCountTest = new IslandCount(mapWithOneIsland, 1, 3);
-	testEqual(islandCountTest, 1);
-
-
-	delete islandCount;
-	delete islandCountWithoutMap;
-	delete islandCountTest;
-
+	
 	system("PAUSE");
 	return 0;
 }
