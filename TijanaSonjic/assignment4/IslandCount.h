52687afe13af22e61985f701e7a3adb1c585a995
Tijana Sonjic <38541577+TijanaSonjic@users.noreply.github.com>
2018-07-07 20:52:00 +0200

    Update IslandCount.h

--- TijanaSonjic/assignment4/IslandCount.h
+++ TijanaSonjic/assignment4/IslandCount.h
@@ -1,29 +1,27 @@
 #pragma once
 class IslandCount{
 private:
-	bool** map;
+	bool** isLand;
 	int height;
 	int width;
 public:
 
 	IslandCount() {}
 
-	IslandCount(bool** matrix, int height, int width) : map(matrix) {
+	IslandCount(bool** matrix, int height, int width) : isLand(matrix) {
 		this->height = height;
 		this->width = width;
 	}
 
-	// Assumption: All four edges of the grid are surrounded by water.
-	//
 	int countIslands() {
 		int num = 0;
 
-		if (!map || height <= 0 || width <= 0)
+		if (!isLand || height <= 0 || width <= 0)
 			return num;
 
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				if (map[i][j]) {
+				if (isLand[i][j]) {
 					mergeAdjacentLands(i, j);
 					num++;
 				}
@@ -34,17 +32,17 @@ public:
 
 	~IslandCount() {
 		for (int i = 0; i < height; i++) {
-			delete[] map[i];
+			delete[] isLand[i];
 		}
-		delete[] map;
+		delete[] isLand;
 	}
 
 private:
 	void mergeAdjacentLands(int i, int j) {
-		if (i < 0 || i >= height || j < 0 || j >= width || map[i][j] == false)
+		if (i < 0 || i >= height || j < 0 || j >= width || isLand[i][j] == false)
 			return;
 
-		map[i][j] = false;
+		isLand[i][j] = false;
 
 		mergeAdjacentLands(i - 1, j);
 		mergeAdjacentLands(i + 1, j);
@@ -52,3 +50,4 @@ private:
 		mergeAdjacentLands(i, j + 1);
 	}
 };
+
