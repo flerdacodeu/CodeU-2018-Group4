b622af8e30ff21ca34c68a0814f8758aea3ab661
ayeletK <kravkrav@gmail.com>
2018-07-08 11:23:10 +0300

    Updated
    Main changes:
    1. operator != was added.
    2. operator + was added.

--- ayeletK/assignment4/position.cpp
+++ ayeletK/assignment4/position.cpp
@@ -4,11 +4,17 @@ using namespace std;
 class Position{
     pair<int,int> _pos;
 public:
-    explicit Position(): _pos() {}
+    explicit Position(): _pos(-1,-1) {}
     Position(int row, int col): _pos(row,col) {}
     int get_row() const { return _pos.first; }
     int get_col() const { return _pos.second; }
     bool operator==(const Position& p) const { return _pos == p._pos; }
+    bool operator!=(const Position& p) const { return _pos != p._pos; }
+    Position operator+(const Position& p) const {
+        int new_row = get_row() + p.get_row();
+        int new_col = get_col() + p.get_col();
+        return Position(new_row, new_col);
+    }
     bool operator<(const Position& p) const { return _pos < p._pos; }
     
 };
