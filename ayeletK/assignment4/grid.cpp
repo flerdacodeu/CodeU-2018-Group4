180c06b554982e8f88484326d82a9fb3681d47b3
ayeletK <kravkrav@gmail.com>
2018-07-08 11:21:48 +0300

    Update (review + bug fixes)
    Main changes:
    1. all usages of "Position*" were changed to "const Position&"
    2. method "update_value" was removed (it had no usage)
    3. method get_neighbours was updated into get_neighbours_of_val. Iteration over neighbours positions was updated.

--- ayeletK/assignment4/grid.cpp
+++ ayeletK/assignment4/grid.cpp
@@ -3,13 +3,15 @@
 #include <unsorted_set>
 #include <stdlib.h>
 
-#define DELTA {-1,0,1}
 using namespace std;
+
+vector<Position> delta_positions = {Position(-1,0), Position(0,-1), Position(1,0), Position(0,1)};
 
 template <typename T>
 class Grid{
     int _rows, _cols;
     vector<vector<T>> _grid;
+    
     bool is_legal_row(int row){
         return row >=0 && row < _rows;
     }
@@ -17,25 +19,17 @@ class Grid{
         return col >=0 && col < _cols;
     }
 public:
-    Grid(): _rows(-1), _cols(-1) {}
-    Grid(int rows_num, int cols_num, bool** data){
-        _rows = rows_num;
-        _cols = cols_num;
-        for (int row = 0; row < _rows; ++row){
-            _grid.push_back(vector<T>(_cols));
-            for (int col = 0; col < _cols; ++col){
-                _grid[row][col] = data[row][col];
-            }
+    Grid(): Grid(nullptr) {}
+    explicit Grid(vector<vector<T>>* data): _rows(-1), _cols(-1) {
+        if (data != nullptr) {
+            _grid = *data;
+            _rows = _grid.size();
+            if (_rows > 0) _cols = _grid[0].size();
         }
     }
-    explicit Grid(vector<vector<T>>* data): _grid(*data) {}
-    void update_value(Position* pos, T val){
-        if (is_legal_position(pos)) 
-            _grid[pos->get_row()][pos->get_col()] = val;
-    }
-    T get_value(Position* pos){
+    T get_value(const Position& pos){
         if (is_legal_position(pos)){
-            return _grid[pos->get_row()][pos->get_col()];
+            return _grid[pos.get_row()][pos.get_col()];
         }
         return T();
     }
@@ -43,19 +37,18 @@ public:
         *rows = _rows;
         *cols = _cols;
     }
-    bool is_legal_position(Position* pos){
-        return is_legal_row(pos->get_row()) && is_legal_col(pos->get_col());
+    bool is_legal_position(const Position& pos){
+        return is_legal_row(pos.get_row()) && is_legal_col(pos.get_col());
     }
-    unordered_set<Position> get_neighbours(Position* pos, T wanted_val){
+    unordered_set<Position> get_neighbours_of_val(const Position& curr_pos, T wanted_val){
         unordered_set<Position> neighbours;
         Position neighbour;
-        for (int i: DELTA){
-            for (int j: DELTA){
-                if (abs(i-j) != 1) continue;
-                neighbour = Position(pos->get_row()+i, pos->get_col()+j);
-                if (is_legal_position(&neighbour) && get_value(&neighbour) == wanted_val)
-                        neighbours.insert(neighbour);
-            }
+        int row, col;
+        for (Position pos: delta_positions){
+            neighbour = curr_pos + pos;
+            if (neighbour == curr_pos) continue;
+            if (get_value(neighbour) == wanted_val)
+                neighbours.insert(neighbour);
         }
         return neighbours;
     }
