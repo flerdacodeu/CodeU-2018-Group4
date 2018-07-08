9c905ad843d74cbef3e39581717d325c902999a0
ayeletK <kravkrav@gmail.com>
2018-07-08 11:31:22 +0300

    Macro addition
    added macro RUN_TEST - prints the name of the test before running it.

---
+++ ayeletK/assignment4/codeu_test_lib.h
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+// Some convenient macros for simple testing.
+// If expectation is fulfilled, prints nothing;
+// If expectation is not fulfilled, prints what went wrong (expectation vs.
+// actual value) with the filename and line number for convenience.
+
+#define EXPECT_TRUE(statement)                                            \
+  if (!(statement)) {                                                     \
+    std::cout << __FILE__ << ":" << __LINE__ << " Failed: "               \
+              << "Statement (" #statement << ") is false, expected true." \
+              << std::endl;                                               \
+  }
+
+#define EXPECT_FALSE(statement)                                           \
+  if (statement) {                                                        \
+    std::cout << __FILE__ << ":" << __LINE__ << " Failed: "               \
+              << "Statement (" #statement << ") is true, expected false." \
+              << std::endl;                                               \
+  }
+
+#define EXPECT_EQ(expected, actual)                                       \
+  if ((expected) != (actual)) {                                           \
+    std::cout << __FILE__ << ":" << __LINE__ << " Failed: "               \
+              << "Statement (" << #actual << ") is " << actual            \
+              << ", expected " << (expected) << " (" << #expected << ")." \
+              << std::endl;                                               \
+  }
+
+#define EXPECT_NULL(statement)                                 \
+  if (statement != nullptr) {                                  \
+    std::cout << __FILE__ << ":" << __LINE__ << " Failed: "    \
+              << "Statement (" << #statement                   \
+              << ") is not null, expected null." << std::endl; \
+  }
+ 
+#define RUN_TEST(test)                                          \
+    cout << "Running " << #test << "..." << endl;               \
+    test()                                                      
+
+// Print operator for vectors. Must be defined in order to use EXPECT_EQ macro
+// with vectors (see example below).
+template <class T>
+inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+  os << "{";
+  for (int i = 0; i < v.size(); i++) {
+    if (i > 0) {
+      os << ", ";
+    }
+    os << v[i];
+  }
+  os << "}";
+}
