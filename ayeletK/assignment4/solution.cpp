ae155d57524d23814228c2b657c962c3d4d87080
ayeletK <kravkrav@gmail.com>
2018-07-08 11:27:05 +0300

    Update (review + bug fixes)
    Main changes:
    1. DFS field was changed to GridComponentsFinder
    2. method collect_all_land_tile_pos was deleted (moved to the responsibility of GridComponentsFinder)
    3. calling count_components instead of find_components.

--- ayeletK/assignment4/solution.cpp
+++ ayeletK/assignment4/solution.cpp
@@ -3,32 +3,17 @@
 #include "dfs.cpp"
 #include "grid.cpp"
 typedef Grid<bool> Map;
+typedef vector<vector<bool>> Map_vec;
 
 class Solution{
     Map _map;
-    DFS<bool> _dfs;
-    
-    // TODO: if I have time - implement iterator for Grid class
-    void collect_all_land_tile_pos(unordered_set<Position>* land_tile){
-        int rows, cols;
-        _map.get_grid_dimentions(&rows, &cols);
-        for (int row = 0; row< rows; ++row){
-            for (int col = 0; col < cols; ++col){
-                Position pos(row,col);
-                if (_map.get_value(&pos) == LAND)
-                    land_tile->insert(pos);
-            }
-        }
-    }
+    GridComponentsFinder<bool> _grid_components_finder;
 public:
-    Solution(): _map(), _dfs(LAND) {}
-    Solution(int rows_num, int cols_num, bool** map): _map(rows_num,cols_num,map), _dfs(LAND) {}
-    int count_islands(){
-        unordered_set<Position> land_tile;
-        collect_all_land_tile_pos(&land_tile);
-        vector<unordered_set<Position>> islands;
-        _dfs.run_DFS(&land_tile, &islands);
-        return islands.size();
+    Solution(): _map(), _grid_components_finder() {}
+    Solution(Map_vec* map_vec): _map(map_vec), _grid_components_finder(LAND) {
+        int i = 0;
     }
-    
+    int count_islands(){
+        return _grid_components_finder.count_components(&_map);
+    }
 };
