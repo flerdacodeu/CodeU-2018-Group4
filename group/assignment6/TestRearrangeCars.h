45e9e5bf4451fe9487912ec5d11a6a535c65e3d5
Tijana Sonjic <38541577+TijanaSonjic@users.noreply.github.com>
2018-07-28 16:52:28 +0200

    Update TestRearrangeCars.h

--- group/assignment6/TestRearrangeCars.h
+++ group/assignment6/TestRearrangeCars.h
@@ -1,14 +1,17 @@
 #ifndef TESTREARRANGECARS_H
 #define TESTREARRANGECARS_H
 
-#include"RearrangeCars.h"
+#include "RearrangeCars.h"
 #include "codeu_test_lib.h"
 
-class TestRearrangeCars{
+class TestRearrangeCars {
 public:
 	void runAllTests();
 private:
 	void testValidRearrangingOfCars();
+	void testValidRearrangingOfCarsWithConstrains();
+	void testValidRearrangingOfCarsWithConstrainsWhenDisplacedCarsCanNotBeMovedToFreeSpace1();
+	void testValidRearrangingOfCarsWithConstrainsWhenDisplacedCarsCanNotBeMovedToFreeSpace2();
 };
 
 #endif
