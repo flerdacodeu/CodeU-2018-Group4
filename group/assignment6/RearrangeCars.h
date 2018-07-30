94cc694000a687d83a402d6201013ea777a1462e
annnufan <annnufan@gmail.com>
2018-07-31 00:40:11 +0300

    make some changes

--- group/assignment6/RearrangeCars.h
+++ group/assignment6/RearrangeCars.h
@@ -19,7 +19,7 @@ public:
 	void setStartStates(const vector<int> &startStates);
 	void generateSequenceOfMoves(const vector<int> &endStates, vector<Move> &sequenceOfMoves);
 	void generateSequenceOfMovesWithConstraints(const vector<int> &endStates, vector<Move> &sequenceOfMoves, const vector<set<int>> &constraints);
-	void generateAllSequencesOfMoves(const vector<int> &endStates, vector<vector<Move>> &sequencesOfMoves);
+	void generateAllSequencesOfMoves(const vector<int> &endStates, const vector<set<int>> &constraints, vector<vector<Move>> &sequencesOfMoves);
 
 private:
 	int findEmptySlotId();
@@ -29,7 +29,7 @@ private:
 	void moveCar(vector<int> &currentStates, vector<int> &carPosition, int from, int to);
 	bool validMove(int carId, int emptySlot, const vector<set<int>>& constrains);
 	int numberOfCarsOnDesiredPosition(const vector<int>& carPosition, const vector<int>& endPosition);
-	void bruteForceAllSequence(vector<int>& currentStates, const vector<int>& endStates, vector<int>& carPosition, int emptySlotId, vector<Move>& sequenceOfMoves, vector<vector<Move>>& sequencesOfMoves, set<vector<int>>& usedPositions);
+	void bruteForceAllSequence(vector<int>& currentStates, const vector<int>& endStates, const vector<set<int>> &constraints, vector<int>& carPosition, int emptySlotId, vector<Move>& sequenceOfMoves, vector<vector<Move>>& sequencesOfMoves, set<vector<int>>& usedPositions);
 };
 
 #endif
