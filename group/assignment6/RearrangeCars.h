eaa5ba609997c082e15380cb1e12affea577ef11
Tijana Sonjic <38541577+TijanaSonjic@users.noreply.github.com>
2018-08-03 01:40:30 +0200

    Renamed function numberOfCarsOnDesiredPositions to numberOfCarsOnTheirDesiredPositions

--- group/assignment6/RearrangeCars.h
+++ group/assignment6/RearrangeCars.h
@@ -18,13 +18,14 @@ public:
 	void setStartStates(const vector<int> &startStates);
 	void generateSequenceOfMoves(const vector<int> &endStates, vector<Move> &sequenceOfMoves, const vector<set<int>> *constraints = nullptr);
 	void generateAllSequencesOfMoves(const vector<int> &endStates, vector<vector<Move>> &sequencesOfMoves, const vector<set<int>> *constraints = nullptr);
+
 private:
 	int findEmptySlotId();
 	vector<int> findCarPositions(const vector<int> &currentStates);
 	int findMisplacedCar(vector<int> &carPosition, vector<int> &currentStates, const vector<int> &endStates, const vector<set<int>> *constraints, vector<Move> &sequenceOfMoves, int &emptySlotId, int &numOfCarsOnDisiredPosition);
 	void moveCar(vector<int> &currentStates, vector<int> &carPosition, int from, int to);
 	bool validMove(int carId, int emptySlot, const vector<set<int>> *constraints);
-	int numberOfCarsOnDesiredPosition(const vector<int> &carPosition, const vector<int> &endPosition);
+	int numberOfCarsOnTheirDesiredPositions(const vector<int> &carPosition, const vector<int> &endPosition);
 	void bruteForceAllSequence(vector<int> &currentStates, const vector<int> &endStates, vector<int> &carPosition, int emptySlotId, vector<Move> &sequenceOfMoves, vector<vector<Move>> &sequencesOfMoves, set<vector<int>> &usedPositions, const vector<set<int>> *constraints = nullptr);
 };
 
