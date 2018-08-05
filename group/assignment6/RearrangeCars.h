83c8e6f4131ec8af70bc926bd1811e36812ea69e
July-Morning <chistiakova.olga@gmail.com>
2018-08-05 20:31:43 +0300

    Changed States to Slot2CarId

--- group/assignment6/RearrangeCars.h
+++ group/assignment6/RearrangeCars.h
@@ -12,21 +12,21 @@
 
 class RearrangeCars {
 private:
-	vector<int> startStates;
+	vector<int> startSlot2CarId;
 public:
-	RearrangeCars(const vector<int> &startStates);
+	RearrangeCars(const vector<int> &startSlot2CarId);
 	~RearrangeCars() = default;
-	void setStartStates(const vector<int> &startStates);
-	bool generateSequenceOfMoves(const vector<int> &endStates, vector<Move> &sequenceOfMoves, const vector<set<int>> *constraints = nullptr);
-	bool generateAllSequencesOfMoves(const vector<int> &endStates, vector<vector<Move>> &sequencesOfMoves, const vector<set<int>> *constraints = nullptr);
+	void setStartSlot2CarId(const vector<int> &startSlot2CarId);
+	bool generateSequenceOfMoves(const vector<int> &endSlot2CarId, vector<Move> &sequenceOfMoves, const vector<set<int>> *constraints = nullptr);
+	bool generateAllSequencesOfMoves(const vector<int> &endSlot2CarId, vector<vector<Move>> &sequencesOfMoves, const vector<set<int>> *constraints = nullptr);
 private:
 	int findEmptySlotId();
-	vector<int> findCarPositions(const vector<int> &currentStates);
-	int findMisplacedCar(vector<int> &carPosition, vector<int> &currentStates, const vector<int> &endStates, const vector<set<int>> *constraints, vector<Move> &sequenceOfMoves, int &emptySlotId, int &numOfCarsOnDisiredPosition);
-	void moveCar(vector<int> &currentStates, vector<int> &carPosition, int from, int to);
+	vector<int> findCarPositions(const vector<int> &currentSlot2CarId);
+	int findMisplacedCar(vector<int> &carPosition, vector<int> &currentSlot2CarId, const vector<int> &endSlot2CarId, const vector<set<int>> *constraints, vector<Move> &sequenceOfMoves, int &emptySlotId, int &numOfCarsOnDisiredPosition);
+	void moveCar(vector<int> &currentSlot2CarId, vector<int> &carPosition, int from, int to);
 	bool validMove(int carId, int emptySlot, const vector<set<int>> *constraints);
 	int numberOfCarsOnTheirDesiredPositions(const vector<int> &carPosition, const vector<int> &endPosition);
-	void getAllSequencesFromCurrentPosition(vector<int> &currentStates, const vector<int> &endStates, vector<int> &carPosition, int emptySlotId, vector<Move> &sequenceOfMoves, vector<vector<Move>> &allSequencesOfMovess, set<vector<int>> &usedPositions, const vector<set<int>> *constraints = nullptr);
+	void getAllSequencesFromCurrentPosition(vector<int> &currentSlot2CarId, const vector<int> &endSlot2CarId, vector<int> &carPosition, int emptySlotId, vector<Move> &sequenceOfMoves, vector<vector<Move>> &allSequencesOfMovess, set<vector<int>> &usedPositions, const vector<set<int>> *constraints = nullptr);
 	bool isInputValid(const vector<int> &endStates, const vector<set<int>> *constraints);
 	bool isInputValidAux(const vector<int> &states, const vector<set<int>> *constraints);
 };
